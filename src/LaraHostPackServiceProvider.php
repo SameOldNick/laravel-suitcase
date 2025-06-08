38d77a0c579ae18b2efc65252eca56f113d9704e
Nick Hamnett <nick@sameoldnick.com>
2025-06-07 20:35:37 -0600

    Update LaraHostPackServiceProvider.php

--- src/LaraHostPackServiceProvider.php
+++ src/LaraHostPackServiceProvider.php
@@ -53,7 +53,7 @@ class LaraHostPackServiceProvider extends ServiceProvider
         }
 
         if (config('hostpack.shared_hosting')) {
-            $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+            $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
         }
     }
 }
